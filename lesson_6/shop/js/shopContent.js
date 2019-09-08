a3b785d90d49b34fdd54832677e16a1959906e01
Albert Farkhutdinov <farhutdinovalbert@yandex.ru>
2019-09-09 01:53:50 +0300

    Домашнее задание №6 к курсу "JavaScript. Уровень 1"

---
+++ lesson_6/shop/js/shopContent.js
@@ -0,0 +1,50 @@
+/* Данный файл предназначен для создания структуры сайта и заполнения основным содержимым. */
+'use strict';
+// определяем структуру каталога
+var catalogSection = cloneObject(section);
+sectionStructure(catalogSection, 'catalog');
+// определяем структуру корзины
+var basketSection = cloneObject(section);
+sectionStructure(basketSection, 'basket');
+var basketSum = createAndAppend('p', basketSection.content, false, basketSection.name + '_sum');
+var basketTable = createAndAppend('table', basketSection.content, false, basketSection.name + '_table');
+// заполняем каталог продуктами
+var numberOfImgForOne = 3; // число картинок для одного товара
+for (var i = 0; i < 6; i++) {
+    var new_product = cloneObject(product);
+    new_product.vendorCode = i + 1;
+    new_product.name = productNames[i];
+    new_product.price = productPrices[i];
+    new_product.smallImgSrc = [];
+    new_product.bigImgSrc = [];
+    for (var j = 1; j <= numberOfImgForOne; j++) {
+        new_product.pushIt(pathToImg + 'small/' + (i + 1) + '.' + j + '.jpg', new_product.smallImgSrc);
+        new_product.pushIt(pathToImg + 'big/' + (i + 1) + '.' + j + '.jpg', new_product.bigImgSrc);
+    }
+    addTo(catalog.list, new_product.vendorCode, new_product.name, new_product.price, new_product.smallImgSrc, new_product.bigImgSrc);
+}
+// работаем с содержимым каталога
+catalogSection.header.innerHTML = 'Каталог';
+var bigWindow = createAndAppend('div', catalogSection.content, false, 'big_window');
+for (var id of ['big_picture_div', 'arrow_left_div', 'arrow_right_div']) {
+    var newDiv = createAndAppend('div', bigWindow, false, id);
+}
+for (var item = 0; item < catalog.list.length; item++) {
+    var catalogProduct = createAndAppend('div', catalogSection.content, catalogSection.name + '_product', false);
+    var catalogImage = createAndAppend('img', catalogProduct, catalogSection.name + '_image', (item + 1) + '.1');
+    catalogImage.src = catalog.list[item].smallImgSrc[0];
+    var catalogTitle = createAndAppend('div', catalogProduct, catalogSection.name + '_title', false);
+    catalogTitle.innerHTML = catalog.list[item].name;
+    var catalogPrice = createAndAppend('div', catalogProduct, catalogSection.name + '_price', false);
+    catalogPrice.innerHTML = catalog.list[item].price + ' руб.';
+    var buyButton = createAndAppend('button', catalogProduct, 'buy_button', (item + 1));
+    buyButton.innerHTML = 'Купить';
+}
+// работаем с содержимым корзины
+basketSection.header.innerHTML = 'Корзина';
+if (basket.list.length == 0) {
+    basketSum.innerHTML = 'Корзина пуста';
+}
+else {
+    basketInfo();
+}
